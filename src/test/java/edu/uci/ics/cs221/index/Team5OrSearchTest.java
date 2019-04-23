98e764b995f955d19af9b0089fdb6e601ae060f4
chenghz4 <chenghz4@uci.edu>
2019-04-23 11:41:46 -0700

    chenghao test

--- src/test/java/edu/uci/ics/cs221/index/Team5OrSearchTest.java
+++ src/test/java/edu/uci/ics/cs221/index/Team5OrSearchTest.java
@@ -6,6 +6,7 @@ import edu.uci.ics.cs221.analysis.PorterStemmer;
 import edu.uci.ics.cs221.analysis.WordBreakTokenizer;
 import edu.uci.ics.cs221.index.inverted.InvertedIndexManager;
 import edu.uci.ics.cs221.storage.Document;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -23,9 +24,13 @@ public class Team5OrSearchTest {
     public void setUp() throws Exception {
         Analyzer analyzer = new ComposableAnalyzer(new WordBreakTokenizer(), new PorterStemmer());
         invertlist = InvertedIndexManager.createOrOpen("src/test/java/edu/uci/ics/cs221/index/Team5OrSearchTest", analyzer);
-        docs = new ArrayList<>();
-        docs.add(new Document("cat dot toy"));
+        docs.add(new Document("cat dog toy"));
         docs.add(new Document("cat Dot"));
+        docs.add(new Document("cat dot toy"));
+        docs.add(new Document("cat toy Dog"));
+        docs.add(new Document(""));
+        docs.add(new Document("cat Dog"));
+
 
         //todo add more docs
     }
@@ -43,6 +48,15 @@ public class Team5OrSearchTest {
             String text = iterator.next().getText();
             assertEquals(true, text.contains("dog") || text.contains("cat"));
         }
+
+    }
+
+
+    @After
+    public void deletetmp() throws Exception{
+        docs.clear();
+        invertlist.flush();
+
 
     }
     //todo test if the query find all matched docs
