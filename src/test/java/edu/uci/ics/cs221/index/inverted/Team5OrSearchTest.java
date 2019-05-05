4ea3f8c4c58256b2bbd2efdc4b9a5e7f3dea2f93
sand <shmsand@gmail.com>
2019-05-05 12:43:41 -0700

    fix

--- src/test/java/edu/uci/ics/cs221/index/inverted/Team5OrSearchTest.java
+++ src/test/java/edu/uci/ics/cs221/index/inverted/Team5OrSearchTest.java
@@ -10,136 +10,141 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class Team5OrSearchTest {
-  private String path = "./index/Team5OrSearchTest";
-  private Analyzer analyzer =
-      new ComposableAnalyzer(new PunctuationTokenizer(), new PorterStemmer());
-  private InvertedIndexManager invertedList;
+    private String path = "./index/Team5OrSearchTest";
+    private Analyzer analyzer = new ComposableAnalyzer(new PunctuationTokenizer(), new PorterStemmer());
+    private InvertedIndexManager invertedList;
 
-  @Before
-  public void setUp() throws Exception {
-    File directory = new File(path);
-    if (!directory.exists()) {
-      directory.mkdirs();
+    @Before
+    public void setUp() throws Exception {
+        File directory = new File(path);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+        invertedList = InvertedIndexManager.createOrOpen(path, analyzer);
+        invertedList.addDocument(new Document("cat dog toy"));
+        invertedList.flush();
+        invertedList.addDocument(new Document("cat Dot"));
+        invertedList.flush();
+        invertedList.addDocument(new Document("cat dot toy"));
+        invertedList.flush();
+        invertedList.addDocument(new Document("cat toy Dog"));
+        invertedList.flush();
+        invertedList.addDocument(new Document("toy dog cat"));
+        invertedList.flush();
+        invertedList.addDocument(new Document("cat Dog"));//docs cannot be null
+        invertedList.flush();
     }
-    invertedList = InvertedIndexManager.createOrOpen(path, analyzer);
-    invertedList.addDocument(new Document("cat dog toy"));
-    invertedList.flush();
-    invertedList.addDocument(new Document("cat Dot"));
-    invertedList.flush();
-    invertedList.addDocument(new Document("cat dot toy"));
-    invertedList.flush();
-    invertedList.addDocument(new Document("cat toy Dog"));
-    invertedList.flush();
-    invertedList.addDocument(new Document("toy dog cat"));
-    invertedList.flush();
-    invertedList.addDocument(new Document("cat Dog")); // docs cannot be null
-    invertedList.flush();
-  }
 
-  // test if multiple keywords work or not. And we set 5 as a threshold for write counter and read
-  // counter,
-  // because I think the number will increase when we call the flush() function and we dont know the
-  // execution order
-  // of test cases, so we set them all to 5.
-  @Test
-  public void Test1() throws Exception {
-    List<String> words = new ArrayList<>();
-    words.add("cat");
-    words.add("dog");
+    // test if multiple keywords work or not. And we set 5 as a threshold for write counter and read counter,
+    // because I think the number will increase when we call the flush() function and we dont know the execution order
+    // of test cases, so we set them all to 5.
+    @Test
+    public void Test1() throws Exception {
+        List<String> words = new ArrayList<>();
+        words.add("cat");
+        words.add("dog");
 
-    Iterator<Document> iterator = invertedList.searchOrQuery(words);
-    int counter = 0;
-    while (iterator.hasNext()) {
-      String text = iterator.next().getText();
-      assertEquals(true, text.contains("dog") || text.contains("cat"));
-      counter++;
+        Iterator<Document> iterator = invertedList.searchOrQuery(words);
+        int counter = 0;
+        while (iterator.hasNext()) {
+            String text = iterator.next().getText();
+            assertEquals(true, text.contains("dog") || text.contains("cat"));
+            counter++;
+        }
+        assertEquals(6, counter);
+        assertTrue(PageFileChannel.readCounter >= 5 && PageFileChannel.writeCounter >= 5);
+        words.clear();
+
     }
-    assertEquals(6, counter);
-    assertTrue(PageFileChannel.readCounter >= 5 && PageFileChannel.writeCounter >= 5);
-    words.clear();
-  }
 
-  // test if single wordkey words works or not
-  @Test
-  public void Test2() throws Exception {
-    List<String> words = new ArrayList<>();
-    words.add("dog");
+    //test if single key words works or not    
+    @Test
+    public void Test2() throws Exception {
+        List<String> words = new ArrayList<>();
+        words.add("dog");
 
-    Iterator<Document> iterator = invertedList.searchOrQuery(words);
-    int counter = 0;
-    while (iterator.hasNext()) {
-      String text = iterator.next().getText();
-      assertEquals(true, text.toLowerCase().contains("dog"));
-      counter++;
+        Iterator<Document> iterator = invertedList.searchOrQuery(words);
+        int counter = 0;
+        while (iterator.hasNext()) {
+            String text = iterator.next().getText();
+            assertEquals(true, text.toLowerCase().contains("dog"));
+            counter++;
+
+        }
+        assertEquals(4, counter);
+        assertTrue(PageFileChannel.readCounter >= 5 && PageFileChannel.writeCounter >= 5);
+        words.clear();
+
     }
-    assertEquals(4, counter);
-    assertTrue(PageFileChannel.readCounter >= 5 && PageFileChannel.writeCounter >= 5);
-    words.clear();
-  }
 
-  // test the case that the wordkey word does not match any file
-  @Test
-  public void Test3() throws Exception {
-    List<String> words = new ArrayList<>();
-    words.add("sdasjdlslsah");
-    words.add("*7&");
-    Iterator<Document> iterator = invertedList.searchOrQuery(words);
-    int counter = 0;
-    while (iterator.hasNext()) {
+    //test the case that the key word does not match any file
+    @Test
+    public void Test3() throws Exception {
+        List<String> words = new ArrayList<>();
+        words.add("sdasjdlslsah");
+        words.add("*7&");
+        Iterator<Document> iterator = invertedList.searchOrQuery(words);
+        int counter = 0;
+        while (iterator.hasNext()) {
 
-      String text = iterator.next().getText();
-      assertEquals(true, text.contains("sdasjdlslsah") || text.contains("*7&"));
-      counter++;
+            String text = iterator.next().getText();
+            assertEquals(true, text.contains("sdasjdlslsah") || text.contains("*7&"));
+            counter++;
+
+        }
+        assertEquals(0, counter);
+        assertTrue(PageFileChannel.readCounter >= 5 && PageFileChannel.writeCounter >= 5);
+        words.clear();
+
     }
-    assertEquals(0, counter);
-    assertTrue(PageFileChannel.readCounter >= 5 && PageFileChannel.writeCounter >= 5);
-    words.clear();
-  }
 
-  // test other words combination
-  @Test
-  public void Test4() throws Exception {
-    List<String> words = new ArrayList<>();
-    words.add("toy");
-    words.add("dog");
-    Iterator<Document> iterator = invertedList.searchOrQuery(words);
-    int counter = 0;
-    while (iterator.hasNext()) {
+    //test other words combination
+    @Test
+    public void Test4() throws Exception {
+        List<String> words = new ArrayList<>();
+        words.add("toy");
+        words.add("dog");
+        Iterator<Document> iterator = invertedList.searchOrQuery(words);
+        int counter = 0;
+        while (iterator.hasNext()) {
 
-      String text = iterator.next().getText();
-      assertEquals(true, text.toLowerCase().contains("dog") || text.toLowerCase().contains("toy"));
-      counter++;
+            String text = iterator.next().getText();
+            assertEquals(true,
+                    text.toLowerCase().contains("dog") || text.toLowerCase().contains("toy"));
+            counter++;
+
+        }
+        assertEquals(5, counter);
+        assertTrue(PageFileChannel.readCounter >= 5 && PageFileChannel.writeCounter >= 5);
+        words.clear();
+
     }
-    assertEquals(5, counter);
-    assertTrue(PageFileChannel.readCounter >= 5 && PageFileChannel.writeCounter >= 5);
-    words.clear();
-  }
 
-  // test empty query list.
-  @Test
-  public void Test5() {
-    List<String> words = new ArrayList<>();
-    Iterator<Document> iterator = invertedList.searchOrQuery(words);
-    assertFalse(iterator.hasNext());
-  }
+    //test empty query list.
+    @Test
+    public void Test5() {
+        List<String> words = new ArrayList<>();
+        Iterator<Document> iterator = invertedList.searchOrQuery(words);
+        assertFalse(iterator.hasNext());
+    }
 
-  @After
-  public void deleteTmp() throws Exception {
-    PageFileChannel.resetCounters();
-    Path rootPath = Paths.get(path);
-    Files.walk(rootPath).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+    @After
 
+    public void deleteTmp() throws Exception {
+        PageFileChannel.resetCounters();
+        File f = new File(path);
+        File[] files = f.listFiles();
+        for (File file : files) {
+            file.delete();
+        }
+        f.delete();
+    }
 
-  }
 }
