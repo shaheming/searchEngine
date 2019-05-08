27aaec3cedecd6150a913d8741250a6b179635eb
sand <shmsand@gmail.com>
2019-05-08 16:32:18 -0700

    addtest

---
+++ src/test/java/edu/uci/ics/cs221/index/positional/Team5IndexCompressionTest.java
@@ -0,0 +1,13 @@
+package edu.uci.ics.cs221.index.positional;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class Team5IndexCompressionTest {
+
+  @Before
+  public void setup() throws Exception {}
+
+  @After
+  public void cleanup() throws Exception {}
+}
