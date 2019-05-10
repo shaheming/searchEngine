0a22d7cc7926125618ad4467c8d712b4b9a60600
chenghz4 <chenghz4@uci.edu>
2019-05-10 16:12:10 -0700

    chenghao

--- src/test/java/edu/uci/ics/cs221/index/positional/Team5IndexCompressionTest.java
+++ src/test/java/edu/uci/ics/cs221/index/positional/Team5IndexCompressionTest.java
@@ -19,6 +19,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.print.Doc;
 import java.lang.reflect.Array;
 
 import static org.junit.Assert.assertFalse;
@@ -76,6 +77,7 @@ public class Team5IndexCompressionTest {
   }
 
   //test simple documents with same text, each key word show only one time each document
+  //mainly test inverted list since inverted list is long but positional list is short
   @Test
   public void Test2() {
     Assert.assertEquals(0, PageFileChannel.readCounter);
@@ -99,11 +101,12 @@ public class Team5IndexCompressionTest {
     int compress_wc=PageFileChannel.writeCounter;
     int compress_rc=PageFileChannel.readCounter;
 
-    Assert.assertTrue(naive_rc>2*compress_rc);
-    Assert.assertTrue(naive_wc>2*compress_wc);
+    Assert.assertTrue(naive_rc>1.5*compress_rc);
+    Assert.assertTrue(naive_wc>1.5*compress_wc);
   }
 
   //test docs with different text and each key word show multiple times in multiple document
+  //mainly test inverted list since inverted list is long but positional list is short
   @Test
   public void Test3() {
     Assert.assertEquals(0, PageFileChannel.readCounter);
@@ -133,11 +136,12 @@ public class Team5IndexCompressionTest {
     int compress_wc=PageFileChannel.writeCounter;
     int compress_rc=PageFileChannel.readCounter;
 
-    Assert.assertTrue(naive_rc>2*compress_rc);
-    Assert.assertTrue(naive_wc>2*compress_wc);
+    Assert.assertTrue(naive_rc>1.5*compress_rc);
+    Assert.assertTrue(naive_wc>1.5*compress_wc);
   }
 
   //test docs with different text and each key word show multiple times only in a document
+  //mainly test positional  list since inverted  since  positional list is long
   @Test
   public void Test4() {
     Assert.assertEquals(0, PageFileChannel.readCounter);
@@ -173,11 +177,56 @@ public class Team5IndexCompressionTest {
     int compress_wc=PageFileChannel.writeCounter;
     int compress_rc=PageFileChannel.readCounter;
 
-    Assert.assertTrue(naive_rc>2*compress_rc);
-    Assert.assertTrue(naive_wc>2*compress_wc);
+    Assert.assertTrue(naive_rc>1.5*compress_rc);
+    Assert.assertTrue(naive_wc>1.5*compress_wc);
   }
+  //test add really long document, testing the positional list works or not
+  //mainly test positional  list since inverted  since  positional list is long
+  public void Test5() {
+    Assert.assertEquals(0, PageFileChannel.readCounter);
+    Assert.assertEquals(0, PageFileChannel.writeCounter);
+    String doc1="cat Dot cat Dog I can not tell the difference between cat and Dog";
+    String doc2="cat and dog have a lot of difference";
+    String doc3="Dog can be very different from cat";
+    for(int i=0;i<1000;i++){
+      doc1=doc1+"cat Dot cat Dog I can not tell the difference between cat and Dog";
+      doc2=doc2+"cat and dog have a lot of difference";
+      doc3=doc3+"Dog can be very different from cat";
+    }
+
+    Document document1=new Document(doc1);
+    Document document2=new Document(doc2);
+    Document document3=new Document(doc3);
 
 
+    for(int i=0;i<30;i++){
+      positional_list_naive_compressor.addDocument(document1);
+      positional_list_naive_compressor.addDocument(document2);
+      positional_list_naive_compressor.addDocument(document3);
+    }
+    positional_list_naive_compressor.flush();
+    for(int i=0;i<positional_list_naive_compressor.getNumSegments();i++){
+      positional_list_naive_compressor.getIndexSegmentPositional(i);
+    }
+    int naive_wc=PageFileChannel.writeCounter;
+    int naive_rc=PageFileChannel.readCounter;
+    PageFileChannel.resetCounters();
+
+    for(int i=0;i<30;i++){
+      positional_list_compressor.addDocument(document1);
+      positional_list_compressor.addDocument(document2);
+      positional_list_compressor.addDocument(document3);
+    }
+    positional_list_compressor.flush();
+    for(int i=0;i<positional_list_compressor.getNumSegments();i++){
+      positional_list_compressor.getIndexSegmentPositional(i);
+    }
+    int compress_wc=PageFileChannel.writeCounter;
+    int compress_rc=PageFileChannel.readCounter;
+
+    Assert.assertTrue(naive_rc>1.5*compress_rc);
+    Assert.assertTrue(naive_wc>1.5*compress_wc);
+  }
 
 
 
